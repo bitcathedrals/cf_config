a2d2a059179c7249f8749deaf47675605a0568c4
Michael Mattie <michael.mattie.employers@gmail.com>
2022-06-21 07:23:54 -0700

    fix(cloud-formation): properly handle regions and boto3 configuration

--- src/cf_config/cloud_formation.py
+++ src/cf_config/cloud_formation.py
@@ -1,13 +1,18 @@
 # copyright (2022) Micheal Mattie  - michael.mattie.employers@gmail.com
 from abc import ABC, abstractmethod
 import os
+
 import boto3
+from botocore.config import Config
+
 from collections import OrderedDict
 import re
 from pprint import pprint
 from time import sleep
 
 from json_decorator.json import json_fn
+
+DEFAULT_REGION = 'us-west-2'
 
 class CloudFormationTemplate(ABC):
     built = None
@@ -55,9 +60,21 @@ class CloudFormationTemplate(ABC):
         pprint(self.built)
 
 
+def cloud_config(region=DEFAULT_REGION, retries=6, mode='standard'):
+    return Config(
+        region_name = region,
+        signature_version = 'v4',
+        retries = {
+            'max_attempts': retries,
+            'mode': mode
+        }
+    )
+
 class CloudFormationExecute:
     environment = None
+
     stack_name = None
+    config = None 
 
     roleARN = None
 
@@ -66,9 +83,11 @@ class CloudFormationExecute:
 
     client = None
 
-    def __init__(self, stack_name, ARN=None):
+    def __init__(self, stack_name, ARN=None, config=cloud_config()):
             self.stack_name = stack_name
             self.roleARN = ARN
+
+            self.config = config
 
             self.access_key = os.environ['CF_ACCESS_KEY']
             self.secret_key = os.environ['CF_SECRET_KEY']
@@ -77,11 +96,12 @@ class CloudFormationExecute:
     def cf_client(self):
         if self.client is None:
             self.client = boto3.client('cloudformation',
+                                       config=self.config,
                                        aws_access_key_id=self.access_key,
                                        aws_secret_access_key=self.secret_key)
         return self.client
 
-    def stack_output(self):
+    def output(self):
         execute = self.cf_client().describe_stacks(StackName=self.stack_name)
 
         if "Outputs" not in execute["Stacks"][0]:
@@ -89,15 +109,13 @@ class CloudFormationExecute:
 
         data = {}
 
-        for kv in execute["Outputs"]:
+        for kv in execute["Stacks"][0]["Outputs"]:
             data[kv['OutputKey']] = kv["OutputValue"]
 
         return data
 
     def status(self):
         execute = self.cf_client().describe_stacks(StackName=self.stack_name)
-
-        print(pprint(execute))
 
         if "StackStatus" in execute["Stacks"][0]:
             return execute["Stacks"][0]["StackStatus"]
@@ -139,6 +157,34 @@ class CloudFormationExecute:
                 print(self.stack_name + " status: " + pending)
 
             sleep(1)
+
+
+def cloud_command(args, template, executor, create_wrapper):
+    command = args[1]
+
+    if command == "print":
+        template.pp()
+        return
+
+    if command == "json":
+        print(template.json())
+        return
+
+    if command == "create":
+        create_wrapper()
+        return
+
+    if command == "status":
+        pprint(executor.status())
+        return
+        
+    if command == "output":
+        pprint(executor.output())
+        return
+
+    print("unkown command: %s" % command)
+
+
 
 
 
