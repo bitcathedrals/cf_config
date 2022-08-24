a489e64ed3cf0ae3d8cc9b908d5bd941fd567109
Michael Mattie <codermattie@runbox.com>
2022-08-24 00:27:47 -0700

    feat(makeconfig): script to run cloudconfig from the command line

---
+++ scripts/makeconfig.py
@@ -0,0 +1,23 @@
+import argparse
+
+from cfconfig.cloud_config import CloudConfig
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Generate configurations from static configuration and AWS CF stacks.")
+
+    parser.add_argument("profile", help="aws credentials profile (required)")
+    parser.add_argument("config", help="config filename (required)")
+    parser.add_argument("environment", help="AWS environment (required)")
+
+    parser.add_argument("--output", help="output filename or STDOUT if not specified")
+
+    args = parser.parse_args()
+    
+    config = CloudConfig(args.profile,
+                         args.config,
+                         args.environment)
+
+    if args.output:
+        config.write_configuration(args.output)
+    else:
+        config.print_configuration()
